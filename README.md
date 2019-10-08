0ee7b3df52f7452c0c1703f38da976a53635b6a2
Nonsensersunny <1074687883@qq.com>
2019-10-08 23:28:53 +0800

    feat:login module completed

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ pip install -r requirements.txt
 
 ## Prerequisites
 - Python >= 3.6
+- [geckodriver](https://github.com/mozilla/geckodriver/releases) should be pre-installed
 
 ## Run
+```shell script
+# up to now, only email login supported, so the login arguments should be valid
+scrapy runspider [spider_name].py -a category=[content to be crawled] -a username=[login username] -a password=[login password]
+```
 
 ## Deployment
 
--- mooc_crawler/mooc_crawler/spiders/mooc.py
+++ mooc_crawler/mooc_crawler/spiders/mooc.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
 
 
 class MoocSpider(scrapy.Spider):
@@ -14,73 +18,47 @@ class MoocSpider(scrapy.Spider):
         'DOWNLOAD_TIMEOUT': 25,
     }
 
-    username = 'zyvenzhao@163.com'
-    password = 'i8AqzObVMz2zt7KwivRK//yH/Fwc5N2vY7rBNb70D69B3oXpnRgENVleuT5APRPwNWsGji8QuxdNcLClUclwqm3hoJheubfSzuVWm/b741iTCicPal9sNWot2K5QlMTsZeY1TY+DXG0FhH/DVxuQLeSWI6DHHRmCQPOeqYXckXE='
+    allowed_domains = ['https://www.icourse163.org']
+    start_urls = ['https://www.icourse163.org']
 
-    header = {
-        "Referer": "https://www.icourse163.org",
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-    }
+    def __init__(self, category=None, username=None, password=None, *args, **kwargs):
+        super(MoocSpider, self).__init__(*args, **kwargs)
+        self.driver = webdriver.Firefox(firefox_options=Options().add_argument('--headless'))
+        self.start_urls = [self.start_urls[0] + f'/category/{category}']
+        self.username = username
+        self.password = password
 
-    allowed_domains = ['https://www.icourse163.org/']
-    start_urls = ['http://https://www.icourse163.org//']
-
+    # do login
     def start_requests(self):
         print("Start mooc crawler")
-        login_url = "https://www.icourse163.org/member/login.htm"
-        login_index_req = scrapy.Request(
-            url=login_url,
-            headers=self.header,
-            callback=self.parse_login_page,
-            dont_filter=True
-        )
-        yield login_index_req
+        login_url = "https://www.icourse163.org/member/login.htm#/webLoginIndex"
+        self.driver.get(login_url)
 
-    def parse_login_page(self, response):
-        print(f"parse_login_page: url = {response.url}")
-        form_post_url = "https://reg.icourse163.org/dl/l"
-        yield scrapy.FormRequest(
-            url=form_post_url,
-            headers=self.header,
-            method="POST",
-            formdata={
-                "un": self.username,
-                "pw": self.password,
-                "channel": 0,
-                "d": 10,
-                "l": 1,
-                "pd": "imooc",
-                "pkid": "cjJVGQM",
-                "pwdKeyUp": 1,
-                "topUTL": "https://www.icourse163.org/home.htm?userId=1027740131#/home/course",
-                "domains": "",
-                "rtid": "",
-                "t": "",
-                "tk": ""
-            },
-            callback=self.parse_login_res,
-            dont_filter=True,
-        )
+        try:
+            self.driver.find_element_by_css_selector("ul.ux-tabs-underline_hd li:nth-of-type(2)").click()
+            print("Mail login method selected")
+        except NoSuchElementException:
+            print("Cannot select mail login method")
+        time.sleep(3)
 
-    def parse_login_res(self, response):
-        print(f"parse_login_res: url = {response.url}")
-        personal_url = "https://www.icourse163.org/user/setting/personInfoEdit.htm#/setting"
-        yield scrapy.Request(
-            url=personal_url,
-            headers=self.header,
-            meta={
-                "dont_redirect": True,
-            },
-            callback=self.parse_login_status,
-            dont_filter=True,
-        )
+        iframe = self.driver.find_element_by_css_selector("div#j-ursContainer-0 iframe")
+        self.driver.switch_to.frame(iframe)
 
-    def parse_login_status(self, response):
-        print(f"parse_login_status: url = {response.url}")
-        yield scrapy.Request(
-            url="https://www.icourse163.org/category/all",
-            headers=self.header,
-        )
+        try:
+            username = self.driver.find_element_by_css_selector("div.u-input input.dlemail")
+            password = self.driver.find_element_by_css_selector("input.dlpwd")
+            login_btn = self.driver.find_element_by_css_selector("div.loginbox a#dologin")
+
+            username.send_keys(self.username)
+            password.send_keys(self.password)
+            login_btn.click()
+        except NoSuchElementException:
+            print("login failed")
+
+        if self.driver.current_url == login_url:
+            yield scrapy.Request(login_url)
+
+        yield scrapy.Request("https://www.icourse163.org/category/computer")
 
     def parse(self, response):
         print(f"parse: url = {response.url}, meta = {response.meta}")
